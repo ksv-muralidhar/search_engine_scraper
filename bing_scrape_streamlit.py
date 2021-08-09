8609ee96746a07642989c6f7b1fa1f628fde8dfb
KSV Muralidhar <murali_dhar0552@yahoo.com>
2021-08-09 09:22:56 +0530

    Update bing_scrape_streamlit.py

--- bing_scrape_streamlit.py
+++ bing_scrape_streamlit.py
@@ -5,21 +5,24 @@ import streamlit as st
 
 st.markdown('<h1>Search Engine Scraper</h1>', unsafe_allow_html=True)
 query = st.text_input('', help='Enter the search string and hit Enter/Return')
-query = query.replace(" ", "+")
-if query:
-    try:
+query = query.replace(" ", "+") #replacing the spaces in query result with +
+
+if query: #Activates the code below on hitting Enter/Return in the search textbox
+    try:#Exception handling 
         req = r.get(f"https://www.bing.com/search?q={query}",
                     headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"})
-        result_str = '<html>'
-        if req.status_code == 200:
-            bs = BeautifulSoup(req.content)
-            search_result = bs.find_all("li", class_="b_algo")
-            search_result = [str(i).replace("<strong>","") for i in search_result]
-            search_result = [str(i).replace("</strong>","") for i in search_result]
-            result_df = pd.DataFrame()
-            for n,i in enumerate(search_result):
-                individual_search_result = BeautifulSoup(i)
-                h2 = individual_search_result.find('h2')
+        result_str = '<html>' #Initializing the HTML code for displaying search results
+        
+        if req.status_code == 200: #Status code 200 indicates a successful request
+            bs = BeautifulSoup(req.content, features="html.parser") #converting the content/text returned by request to a BeautifulSoup object
+            search_result = bs.find_all("li", class_="b_algo") #'b_algo' is the class of the list object which represents a single result
+            search_result = [str(i).replace("<strong>","") for i in search_result] #removing the <strong> tag
+            search_result = [str(i).replace("</strong>","") for i in search_result] #removing the </strong> tag
+            result_df = pd.DataFrame() #Initializing the data frame that stores the results
+            
+            for n,i in enumerate(search_result): #iterating through the search results
+                individual_search_result = BeautifulSoup(i, features="html.parser") #converting individual search result into a BeautifulSoup object
+                h2 = individual_search_result.find('h2') 
                 href = h2.find('a').get('href')
                 url_txt = h2.find('a').text
                 description = individual_search_result.find('p').text
@@ -36,6 +39,7 @@ if query:
             result_df = pd.DataFrame({"Title": "", "URL": "", "Description": ""}, index=[0])
             result_str = '<html></html>'
             count_str = '<b style="font-size:20px;">Looks like an error!!</b>'
+    
     except:
         result_df = pd.DataFrame({"Title": "", "URL": "", "Description": ""}, index=[0])
         result_str = '<html></html>'
